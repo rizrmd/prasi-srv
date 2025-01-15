5b82c6de76dd64418122f91c67c12f02251a7225
Rizky <rizky@avolut.com>
2025-01-15 19:14:55 +0700

    fix

--- standalone/frontend.tsx
+++ standalone/frontend.tsx
@@ -1,6 +1,10 @@
+import { css } from "goober";
+import dayjs from "dayjs";
 import type { ReactElement } from "react";
 
 export const initFrontend = (app: { layout: ReactElement }) => {
   const w = window as any;
+  w.css = css;
+  w.dayjs = dayjs;
   w.prasi_root = app.layout;
 };
